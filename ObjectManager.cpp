c07f271a3c62238df4a1788ec3e19c65012d9214
kokojae <54296776+kokojae@users.noreply.github.com>
2020-04-10 09:29:50 +0900

    ObjectManager.h 완성
    ObjectManager.h 완성함 나머지 클래스들 만들고 ObjectManager.cpp완성해야함

---
+++ ObjectManager.cpp
@@ -0,0 +1,38 @@
+#include "DXUT.h"
+#include "ObjectManager.h"
+#include "EngineTry2.h"
+
+void ObjectManager::Update()
+{
+}
+
+void ObjectManager::LateUpdate()
+{
+}
+
+void ObjectManager::Render()
+{
+}
+
+void ObjectManager::Release()
+{
+}
+
+void ObjectManager::Clear()
+{
+}
+
+GameObject* ObjectManager::ColliderCheck(RECT* objectRect, int layer)
+{
+	return nullptr;
+}
+
+GameObject* ObjectManager::SearchObject(std::wstring objectName)
+{
+	return nullptr;
+}
+
+bool Sort(const GameObject* a, const GameObject* b)
+{
+	return false;
+}
