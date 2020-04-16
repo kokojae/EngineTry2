0e5679cd0c41a5e87e8d1de8202e6ab974ee2c05
kokojae <54296776+kokojae@users.noreply.github.com>
2020-04-16 14:49:48 +0900

    Enemy 수정
    Enemy클래스가 점수를 가지고있게 수정, 점수 부여 구현

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,2 +1,17 @@
 #include "DXUT.h"
 #include "Enemy.h"
+#include "EngineTry2.h"
+
+void Enemy::Damaged(float damage, Bullet* bullet)
+{
+	hp -= damage;
+	if (hp <= 0)
+	{
+		destroy = true;
+		Player* player = dynamic_cast<Player*>(bullet->launcher);
+		if (player != nullptr)
+		{
+			player->point = point;
+		}
+	}
+}
--- Enemy.h
+++ Enemy.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "GameObject.h"
+class Bullet;
+
 class Enemy :
 	public GameObject
 {
 public:
+	float hp;
+	int point;
 
+	virtual void Damaged(float damage, Bullet* bullet);
 };
 
